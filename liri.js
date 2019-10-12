83a01e8722f2b6f5027890fa263e68017ceb4eab
Sean Michael Shea <seanshea1337@gmail.com>
2019-10-12 17:58:11 -0400

    set up vars and switch statement

---
+++ liri.js
@@ -0,0 +1,24 @@
+// require
+require("dotenv").config();
+
+// vars
+let keys = require("./keys.js");
+let spotifyRequire = require('node-spotify-api');
+let spotify = new spotifyRequire(keys.spotify);
+let fs = require('fs');
+
+var command = process.argv[2]; 
+switch (command) {
+    case "concert-this":
+        concertThis(value);
+        break;
+    case "spotify-this-song":
+        spotifySong(value);
+        break;
+    case "movie-this":
+        movieThis(value);
+        break;
+    case "do-what-it-says":
+        doThis(value);
+        break;
+};localStorage
